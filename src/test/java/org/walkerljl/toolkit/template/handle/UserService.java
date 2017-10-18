ed7bd8e5ae7a0e1b166728a64d12d2df5dbcd127
walkerljl <walkerljlx@163.com>
2017-10-18 14:54:41 +0800

    去掉文档签名注释

--- src/test/java/org/walkerljl/toolkit/template/handle/UserService.java
+++ src/test/java/org/walkerljl/toolkit/template/handle/UserService.java
@@ -1,13 +1,8 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2017 All Rights Reserved.
- */
 package org.walkerljl.toolkit.template.handle;
 
 /**
  *
- * @author junlin.ljl
- * @version $Id: UserService.java, v 0.1 2017年10月17日 下午3:40 junlin.ljl Exp $
+ * @author lijunlin
  */
 public interface UserService {
 
